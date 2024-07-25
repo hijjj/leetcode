cf46794285d259cb361ee32bc709c3f9354ca886
wanwei <wansandawang@163.com>
2024-07-25 22:28:52 +0800

    算法刷题提交

--- src/哈希表/IsAnagram.java
+++ src/哈希表/IsAnagram.java
@@ -1,7 +1,23 @@
 package 哈希表;
 
 public class IsAnagram {
-    public boolean isAnagram(String s, String t) {
+    int[] record = new int[26];
 
+    public boolean isAnagram(String s, String t) {
+        char[] chars1 = s.toCharArray();
+        char[] chars2 = t.toCharArray();
+        for (char c : chars1) {
+            record[c - 'a']++;
+        }
+        for (char c : chars2) {
+            record[c - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (record[i] != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
