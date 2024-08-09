9f054cab31a3ec2a22f2f8335159fe8133bba8da
wanwei <wansandawang@163.com>
2024-08-09 08:18:08 +0800

    算法刷题提交

--- src/huaweiOd/SmartScoreTable.java
+++ src/huaweiOd/SmartScoreTable.java
@@ -1,88 +1,72 @@
 package huaweiOd;
 
-import java.util.*;
-import java.util.stream.Collectors;
-//智能成绩 good
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.StringJoiner;
+//智能成绩表 good
 public class SmartScoreTable {
-    public void smartScoreTable(List<String> lists) {
-        Comparator<String> comparator = new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                Integer score1 = Integer.valueOf(o1.split("-")[1]);
-                Integer score2 = Integer.valueOf(o2.split("-")[1]);
-                if (score1.compareTo(score2) != 0) {
-                    return score2.compareTo(score1);
-                } else {
-                    String name1 = o1.split("-")[0];
-                    String name2 = o2.split("-")[0];
-                    char[] c1 = name1.toCharArray();
-                    char[] c2 = name2.toCharArray();
-                    for (int i = 0; ; i++) {
-                        if (c1.length > i && c2.length > i) {
-                            if (c1[i] == c2[i]) {
-                            } else {
-                                return c2[i] - c1[i];
-                            }
-                        } else {
-                            return c2.length - c1.length;
-                        }
-                    }
-                }
-            }
-        };
-        Collections.sort(lists, comparator);
+    static class Student {
+        String name; // 学生姓名
+        int[] rank; // 排名要素
 
-        if (!lists.isEmpty()) {
-            for(String str:lists){
-                System.out.print(str.split("-")[0]+" ");
-            }
+        public Student(String name, int[] rank) {
+            this.name = name;
+            this.rank = rank;
         }
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String line = scanner.nextLine();
-        int studentsNums = Integer.parseInt(line.split(" ")[0]);
-        int projectNums = Integer.parseInt(line.split(" ")[1]);
-        String line1 = scanner.nextLine();
-        String[] line1s = line1.split(" ");
-        List<String> projectLists = Arrays.asList(line1s);
-        Map<String, List<Integer>> maps = new HashMap<>();
-        for (int i = 0; i < studentsNums; i++) {
-            String line2 = scanner.nextLine();
-            String[] line2s = line2.split(" ");
-            List<Integer> lists = new ArrayList<>();
-            for (int j = 1; j < line2s.length; j++) {
-                lists.add(Integer.valueOf(line2s[j]));
-            }
-            maps.put(line2s[0], lists);
+        Scanner sc = new Scanner(System.in);
+
+        // 学生人数
+        int n = sc.nextInt();
+
+        // 科目数量
+        int m = sc.nextInt();
+
+        // key是科目名称，val是科目出现顺序的序号
+        HashMap<String, Integer> subject_rankIdx = new HashMap<>();
+        for (int i = 0; i < m; i++) {
+            subject_rankIdx.put(sc.next(), i);
         }
-        String xueke = scanner.nextLine();
-        SmartScoreTable smartScoreTable = new SmartScoreTable();
-        List<String> lists2 = new ArrayList<>();
-        if (!projectLists.contains(xueke)) {
-            for (String key : maps.keySet()) {
-                List<Integer> lists = maps.get(key);
-                int count = lists.stream().mapToInt(u -> u).sum();
-                String str = key + "-" + count;
-                lists2.add(str);
+
+        ArrayList<Student> students = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            // 学生姓名
+            String name = sc.next();
+
+            // rank记录学生排名的要素（0~m-1索引上记录的是各科成绩，m索引上记录的是总分）
+            int[] rank = new int[m + 1];
+            // 学生的总分
+            int score_sum = 0;
+
+            // m 个科目成绩
+            for (int j = 0; j < m; j++) {
+                rank[j] = sc.nextInt();
+                score_sum += rank[j];
             }
-        } else {
-            int i = 0;
-            for (String str : projectLists) {
-                if (xueke.equals(str)) {
-                     break;
-                }
-            }
-            for(String key:maps.keySet()){
-                List<Integer> lists = maps.get(key);
-                if(i>=lists.size()){
-                    continue;
-                }
-                String str = key + "-" + lists.get(i);
-                lists2.add(str);
-            }
+
+            rank[m] = score_sum;
+
+            students.add(new Student(name, rank));
         }
-        smartScoreTable.smartScoreTable(lists2);
+
+        // 输入用作排名的科目名称
+        // 根据用作排名的科目名称获取对应排名要素序号，如果不存在，则按总分排序，排名要素序号取m
+        int rankIdx = subject_rankIdx.getOrDefault(sc.next(), m);
+
+        students.sort(
+                (a, b) ->
+                        a.rank[rankIdx] != b.rank[rankIdx]
+                                ? b.rank[rankIdx] - a.rank[rankIdx]
+                                : a.name.compareTo(b.name));
+
+        StringJoiner sj = new StringJoiner(" ");
+        for (Student student : students) {
+            sj.add(student.name);
+        }
+
+        System.out.println(sj);
     }
 }
